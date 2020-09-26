39ffa58ea191e6179ab5bf72134ef98ce14c9c5f
Horst Schneider <horst.schneider@posteo.de>
2020-09-26 18:12:43 +0200

    Add divmod and tests

--- juiltins.js
+++ juiltins.js
@@ -1,5 +1,46 @@
+/**
+ * JavaScript-Lol: The modulo bug.
+ * https://dustinpfister.github.io/2017/09/02/js-whats-wrong-with-modulo/
+ * 
+ * Or is it a bug?
+ * http://python-history.blogspot.com/2010/08/why-pythons-integer-division-floors.html
+ */
+function mod(a, b) {
+  return (a % b + b) % b;
+}
+
+class ZeroDivisionError extends Error {
+  constructor() {
+    super('ZeroDivisionError: integer division or modulo by zero');
+  }
+}
+
 function abs(n) {
   return Math.abs(n);
 }
 
-module.exports = { abs };
+function any(iterable) {
+  return iterable.some(v => !!v);
+}
+
+function all(iterable) {
+  return iterable.every(v => !!v);
+}
+
+function dir(obj) {
+  console.dir(obj);
+}
+
+function divmod(a, b) {
+  if (b === 0) {
+    throw new ZeroDivisionError();
+  }
+
+  const quotient =  Math.floor(a / b);
+  const remainder = mod(a, b);
+  
+  return [ quotient, remainder ];
+}
+
+
+module.exports = { abs, all, any, dir, divmod, ZeroDivisionError };
--- juiltins.spec.js
+++ juiltins.spec.js
@@ -1,4 +1,10 @@
-const { abs } = require("./juiltins");
+const { 
+  abs,
+  all,
+  any,
+  divmod,
+  ZeroDivisionError
+} = require("./juiltins");
 
 describe('juiltins', () => {
   describe('abs', () => {
@@ -22,4 +28,70 @@ describe('juiltins', () => {
       expect.fail('Not Implemented: complex');
     });
   })
-})
+
+  describe('any', () => {
+    [
+      { value: [], expected: false },
+      { value: [0], expected: false },
+      { value: [0, null, undefined, ""], expected: false },
+      { value: [1], expected: true },
+      { value: [1, 2, 3], expected: true },
+      { value: [0, 2, null], expected: true },
+    ].forEach(({ value, expected }) => {
+      it(`any(${JSON.stringify(value)}) === ${expected}`, () => {
+        expect(any(value)).toBe(expected);
+      });
+    })
+  });
+
+  describe('all', () => {
+    [
+      { value: [], expected: true },
+      { value: [1], expected: true },
+      { value: [1, 2, 3], expected: true },
+      { value: [null], expected: false },
+      { value: [1, 2, 3, null], expected: false },
+    ].forEach(({ value, expected }) => {
+      it(`all(${JSON.stringify(value)}) === ${expected}`, () => {
+        expect(all(value)).toBe(expected);
+      });
+    })
+  });
+
+  describe('divmod', () => {
+    [
+      { a: 0, b: 1, expected: [0, 0] },
+      { a: 0, b: 2, expected: [0, 0] },
+      { a: 1, b: 1, expected: [1, 0] },
+      { a: 2, b: 1, expected: [2, 0] },
+      { a: 3, b: 4, expected: [0, 3] },
+      { a: 1.5, b: 3.2, expected: [0.0, 1.5] },
+    ].forEach(({ a, b, expected }) => {
+      it(`divmod(${a}, ${b}) === ${expected}`, () => {
+        expect(divmod(a, b)).toEqual(expected);
+      });
+    });
+
+    it('throws ZeroDivisionError when b = 0', () => {
+      // Arrange
+      const a = 10;
+      const b = 0;
+
+      // Act
+      expect(() => divmod(a, b)).toThrow(ZeroDivisionError);
+    });
+
+    it('mimics Pythons modulo behavior for negative numbers', () => {
+      // Arrange
+      const a = -200.85;
+      const b = 17.56;
+
+      // Act
+      const [ quotient, remainder ] = divmod(a, b);
+      
+      // Assert
+      expect(quotient).toEqual(-12.0);
+      expect(remainder).toEqual(9.86999999999999);
+    })
+  });
+});
